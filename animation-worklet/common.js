5025891041538a4d02e454ac7bf61b083666d8e4
Majid Valipour <majidvp@chromium.org>
2019-01-14 10:51:02 -0800

    [Animation Worklet] clean up wpt tests
    - Fix a test that should have been an async test but was not.
    - Use existing web-animation common utils where applicable.
    - Introduce and use common worklets (passthrough, constant_time) for testing.
    
    Bug:915352
    Change-Id: I5f503bebe3bc4be4f3c76a84604df63a905439e2

--- animation-worklet/common.js
+++ animation-worklet/common.js
@@ -8,26 +8,18 @@ function registerPassthroughAnimator() {
   `);
 }
 
+function registerConstantTimeAnimator(localTime) {
+  return runInAnimationWorklet(`
+    registerAnimator('constant_time', class {
+      animate(currentTime, effect) { effect.localTime = ${localTime}; }
+    });
+  `);
+}
+
+
 function runInAnimationWorklet(code) {
   return CSS.animationWorklet.addModule(
     URL.createObjectURL(new Blob([code], {type: 'text/javascript'}))
   );
 }
 
-function waitForAnimationFrames(count, callback) {
-  function rafCallback() {
-    if (count <= 0) {
-      callback();
-    } else {
-      count -= 1;
-      window.requestAnimationFrame(rafCallback);
-    }
-  }
-  rafCallback();
-};
-
-// Wait for two main thread frames to guarantee that compositor has produced
-// at least one frame. Note that this is a Chrome-only concept.
-function waitTwoAnimationFrames(callback) {
-  waitForAnimationFrames(2, callback);
-};
